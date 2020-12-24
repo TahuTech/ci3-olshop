d16e13022e23e244a55858df7d4e4d6610f21580
ALi <ali.sumberejo1@gmail.com>
2020-12-24 16:02:36 +0700

    tutor 37 table transaksi

--- application/views/v_checkout.php
+++ application/views/v_checkout.php
@@ -127,7 +127,7 @@
             <div class="table-responsive">
                 <table class="table">
                     <tr>
-                        <th style="width:50%">Subtotal:</th>
+                        <th style="width:50%">Grand Total:</th>
                         <th>
                             Rp. <?php echo number_format($this->cart->total(), 0); ?>
                         </th>
